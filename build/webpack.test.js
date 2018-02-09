39ca2c30909510d282b004bf6bb99a28c449c931
Hyounwoo <hyounoosung@gmail.com>
2018-02-09 19:58:49 +0900

    test

--- build/webpack.test.js
+++ build/webpack.test.js
@@ -1,44 +1,31 @@
 var path = require('path')
 var webpack = require('webpack')
-
+ 
 module.exports = {
   entry: './dev/index.js',
   output: {
     path: path.resolve(__dirname, '../docs'),
     publicPath: '/docs/',
-    filename: 'v-treeview.min.js',
-    library: 'VTreeview',
-    libraryTarget: 'umd',
-    umdNamedDefine: true
-  },
-  externals: {
-    "vue": "Vue"
+    filename: 'build.js'
   },
   module: {
-    rules: [{
-      test: /\.vue$/,
-      loader: 'vue-loader',
-    }, {
-      test: /\.js$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/
-    }, ]
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+    ]
   },
-  plugins: [
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-      debug: false
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      include: /\.min\.js$/,
-      compress: {
-        warnings: false
-      }
-    })
-  ],
+  devServer: {
+    noInfo: true
+  },
   performance: {
-    hints: "error"
+    hints: false
   },
-  devtool: 'source-map'
+  devtool: '#eval-source-map'
 }
